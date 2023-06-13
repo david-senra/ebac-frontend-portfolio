0550dcab370584daf0b6db6643c266d960a94441
David Senra <davidsenra33@gmail.com>
2023-06-12 22:46:40 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/david-senra.png" />
-    <Titulo fontSize={20}>David Senra</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>David Senra</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        david-senra
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
